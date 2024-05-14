11bcaf848b2c65b02beff55729530e17de697fd1
dinnddaaaa <adindadindaftz@gmail.com>
2024-05-14 19:39:19 +0700

    register succes

--- loginpage/proses_register.php
+++ loginpage/proses_register.php
@@ -2,14 +2,14 @@
     //memanggil file koneksi.php
     include "koneksi.php";
     $username=$_POST['username'];
+    $email=$_POST['email'];
     $password=$_POST['password'];
-    $nama=$_POST['nama'];
     $level="user";//level otomatis diisi user pd saat registrasi
     
     //format acak password harus sama dengan proses_login.php
     $pengacak="p3ng4c4k";
     $password_acak=md5($pengacak.md5($password).$pengacak);
-    $kirim=$_POST['kirim'];
+    $kirim=$_POST['kirim_register'];
 
     //proses kirim data ke database MYSQL
     if($kirim){
