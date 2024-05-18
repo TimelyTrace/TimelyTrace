ffc1f0b7556c881c4254083085b93336371b1fee
dinnddaaaa <adindadindaftz@gmail.com>
2024-05-19 01:34:25 +0700

    CRUD edit done, succes

--- input/proses_input.php
+++ input/proses_input.php
@@ -20,10 +20,9 @@ if(isset($_POST['submit'])) {
     $query = "UPDATE `tb_input` SET nama='$nama', kelas='$kelas', absen='$absen', tanggal='$tanggal', alasan='$alasan' WHERE nisn='$nisn'";
     $hasil = mysqli_query($conn, $query);
 
-<<<<<<< HEAD
     if($hasil) {
         echo "Data berhasil diperbarui.";
-=======
+
     // Menjalankan query dan memeriksa hasilnya
     if ($conn->query($query) === TRUE) {
         echo "Data berhasil masuk database<br>";
@@ -33,10 +32,9 @@ if(isset($_POST['submit'])) {
         echo $tanggal . "<br>";
         echo $alasan . "<br>";
         echo "<a href='../CRUD/Datapage.php'>ke CRUD</a>";
->>>>>>> 568e710a2cf5d7d119277e5d8e51b7859ee2384c
     } else {
         echo "Gagal memperbarui data: " . mysqli_error($conn);
     }
 }
-?>
+}
 ?>
